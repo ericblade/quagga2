9b1e261429c54c7169877255c38e9da5dd838d89
Eric Blade <blade.eric@gmail.com>
2020-04-26 04:39:38 -0400

    further breakup of setupInputStream

--- src/quagga/setupInputStream.ts
+++ src/quagga/setupInputStream.ts
@@ -1,31 +1,44 @@
+import InputStream from "../input/input_stream";
+
 export type InputStreamType = 'VideoStream' | 'ImageStream' | 'LiveStream';
 
+function createVideoStream() {
+    const video = document.createElement('video');
+    return {
+        video,
+        inputStream: InputStream.createVideoStream(video),
+    };
+}
+
+function createImageStream() {
+    return { inputStream: InputStream.createImageStream() };
+}
+
+function createLiveStream(viewport: Element) {
+    let video: HTMLVideoElement | null = null;
+    if (viewport) {
+        video = viewport.querySelector('video');
+        if (!video) {
+            video = document.createElement('video');
+            viewport.appendChild(video);
+        }
+    }
+    return {
+        video,
+        inputStream: InputStream.createLiveStream(video),
+    };
+}
+
+
 // TODO: need to create an InputStream typescript interface, so we don't have an "any" in the next line
-export default function setupInputStream(type: InputStreamType, viewport: HTMLElement, InputStream: any) {
+export default function setupInputStream(type: InputStreamType, viewport: Element) {
     switch (type) {
-        case 'VideoStream': {
-            const video = document.createElement('video');
-            return {
-                video,
-                inputStream: InputStream.createVideoStream(video),
-            };
-        }
+        case 'VideoStream':
+            return createVideoStream();
         case 'ImageStream':
-            return { inputStream: InputStream.createImageStream() };
-        case 'LiveStream': { // TODO: test to see what happens if you run in node and ask for LiveStream, it probably fails spectacularly, and should fail gracefully
-            let video: HTMLVideoElement | null = null;
-            if (viewport) {
-                video = viewport.querySelector('video');
-                if (!video) {
-                    video = document.createElement('video');
-                    viewport.appendChild(video);
-                }
-            }
-            return {
-                video,
-                inputStream: InputStream.createLiveStream(video),
-            };
-        }
+            return createImageStream();
+        case 'LiveStream':
+            return createLiveStream(viewport);
         default:
             console.error(`* setupInputStream invalid type ${type}`);
             return { video: null, inputStream: null };
