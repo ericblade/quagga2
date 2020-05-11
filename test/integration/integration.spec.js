5cffef0eb3509ead6f1e32ed4b36b6d3e978c181
Eric Blade <blade.eric@gmail.com>
2020-05-11 05:04:23 -0400

    move code_128 tests

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -139,48 +139,6 @@ describe('decodeSingle', function () {
 
     // TODO: write a test that tests Promise return/resolve
 
-    describe('Code128', function() {
-        var config = {
-                inputStream: {
-                    size: 800,
-                    singleChannel: false,
-                },
-                locator: {
-                    patchSize: 'medium',
-                    halfSample: true,
-                },
-                numOfWorkers: 0,
-                decoder: {
-                    readers: ['code_128_reader'],
-                },
-                locate: true,
-                src: null,
-            },
-            testSet = [
-                {'name': 'image-001.jpg', 'result': '0001285112001000040801'},
-                {'name': 'image-002.jpg', 'result': 'FANAVF14617104'},
-                {'name': 'image-003.jpg', 'result': '673023'},
-                {'name': 'image-004.jpg', 'result': '010210150301625334'},
-                {'name': 'image-005.jpg', 'result': '419055603900009001012999'},
-                {'name': 'image-006.jpg', 'result': '419055603900009001012999'},
-                {'name': 'image-007.jpg', 'result': '420957479499907123456123456781'},
-                {'name': 'image-008.jpg', 'result': '1020185021797280784055'},
-                {'name': 'image-009.jpg', 'result': '0001285112001000040801'},
-                {'name': 'image-010.jpg', 'result': '673023'},
-                // TODO: need to implement having different inputStream parameters to be able to
-                // read this one -- it works only with inputStream size set to 1600 presently, but
-                // other samples break at that high a size.
-                // { name: 'image-011.png', result: '33c64780-a9c0-e92a-820c-fae7011c11e2' },
-            ];
-
-        testSet.forEach(function(sample) {
-            sample.format = 'code_128';
-        });
-
-        config.decoder.readers = ['code_128_reader'];
-        _runTestSet(testSet, config);
-    });
-
     describe('Code128 run as external', function() {
         var config = {
                 inputStream: {
--- test/integration/newintegration.spec.ts
+++ test/integration/newintegration.spec.ts
@@ -60,6 +60,8 @@ describe.only('End-To-End Decoder Tests', () => {
         { 'name': 'image-009.jpg', 'result': '9004593978587', format: 'ean_13' },
         { 'name': 'image-010.jpg', 'result': '9002244845578', format: 'ean_13' },
     ]);
+    // TODO: note that the FORMAT reported from a supplement equals the parent. What exactly is the
+    // difference between a supplement and a separate reader?  is it just semantic?
     runDecoderTest('ean_extended', generateConfig({
         inputStream: {
             size: 800,
@@ -87,5 +89,26 @@ describe.only('End-To-End Decoder Tests', () => {
         { 'name': 'image-008.jpg', 'result': '419871600890101', format: 'ean_13' },
         { 'name': 'image-009.jpg', 'result': '978054466825652495', format: 'ean_13' },
         { 'name': 'image-010.jpg', 'result': '900437801102701', format: 'ean_13' },
+    ]);
+    runDecoderTest('code_128', {
+        inputStream: {
+            size: 800,
+            singleChannel: false,
+        }
+    }, [
+        { 'name': 'image-001.jpg', 'result': '0001285112001000040801', format: 'code_128' },
+        { 'name': 'image-002.jpg', 'result': 'FANAVF14617104', format: 'code_128' },
+        { 'name': 'image-003.jpg', 'result': '673023', format: 'code_128' },
+        { 'name': 'image-004.jpg', 'result': '010210150301625334', format: 'code_128' },
+        { 'name': 'image-005.jpg', 'result': '419055603900009001012999', format: 'code_128' },
+        { 'name': 'image-006.jpg', 'result': '419055603900009001012999', format: 'code_128' },
+        { 'name': 'image-007.jpg', 'result': '420957479499907123456123456781', format: 'code_128' },
+        { 'name': 'image-008.jpg', 'result': '1020185021797280784055', format: 'code_128' },
+        { 'name': 'image-009.jpg', 'result': '0001285112001000040801', format: 'code_128' },
+        { 'name': 'image-010.jpg', 'result': '673023', format: 'code_128' },
+        // TODO: need to implement having different inputStream parameters to be able to
+        // read this one -- it works only with inputStream size set to 1600 presently, but
+        // other samples break at that high a size.
+        // { name: 'image-011.png', result: '33c64780-a9c0-e92a-820c-fae7011c11e2' },
     ]);
 });
