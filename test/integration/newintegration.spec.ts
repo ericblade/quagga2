7c65bf9a8399ddb00d2b87ad8836978d3d0ea507
Eric Blade <blade.eric@gmail.com>
2020-05-12 01:16:46 -0400

    move 2of5 tests

--- test/integration/newintegration.spec.ts
+++ test/integration/newintegration.spec.ts
@@ -258,4 +258,26 @@ describe.only('End-To-End Decoder Tests', () => {
             { 'name': 'image-005.jpg', 'result': '2167361334', format: 'i2of5' },
         ]
     );
+    runDecoderTest(
+        '2of5',
+        generateConfig({
+            inputStream: { size: 800, singleChannel: false },
+            decoder: {
+                readers: ['2of5_reader'],
+            },
+        }),
+        [
+            { 'name': 'image-001.jpg', 'result': '9577149002', format: '2of5' },
+            { 'name': 'image-002.jpg', 'result': '9577149002', format: '2of5' },
+            { 'name': 'image-003.jpg', 'result': '5776158811', format: '2of5' },
+            { 'name': 'image-004.jpg', 'result': '0463381455', format: '2of5' },
+            { 'name': 'image-005.jpg', 'result': '3261594101', format: '2of5' },
+            { 'name': 'image-006.jpg', 'result': '3261594101', format: '2of5' },
+            { 'name': 'image-007.jpg', 'result': '3261594101', format: '2of5' },
+            { 'name': 'image-008.jpg', 'result': '6730705801', format: '2of5' },
+            { 'name': 'image-009.jpg', 'result': '5776158811', format: '2of5' },
+            { 'name': 'image-010.jpg', 'result': '5776158811', format: '2of5' },
+        ]
+    );
+
 });
