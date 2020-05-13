2057e5b780e5767b5e774034af79474913da358e
Eric Blade <blade.eric@gmail.com>
2020-05-13 18:04:22 -0400

    remove some more bits that were leftover from the old test system

--- test/test-main.js
+++
@@ -1,8 +0,0 @@
-require('events').EventEmitter.prototype._maxListeners = 0;
-require('core-js/es');
-
-const testsContext = require.context("./spec", true, /.*(t|j)sx?$/);
-testsContext.keys().forEach(testsContext);
-
-const componentsContext = require.context('../src/', true, /\.*(t|j)sx?$/);
-componentsContext.keys().forEach(componentsContext);
