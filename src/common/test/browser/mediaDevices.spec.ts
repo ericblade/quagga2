3ed9b16968f39e08806ebbab8c45917c76f854f5
Eric Blade <blade.eric@gmail.com>
2025-11-26 03:32:31 -0500

    pick up some of the TODO tests in the mediaDevices, camera_access, improve tests in exif_helper

--- src/common/test/browser/mediaDevices.spec.ts
+++ src/common/test/browser/mediaDevices.spec.ts
@@ -4,7 +4,18 @@ import { enumerateDevices, getUserMedia } from '../../mediaDevices';
 
 describe('mediaDevices (browser)', () => {
     describe('enumerateDevices', () => {
-        it('TODO: rejects with an error if enumerateDevices is not supported in browser version');
+        it('rejects with an error if enumerateDevices is not supported in browser version', async () => {
+            const origEnumerateDevices = navigator.mediaDevices.enumerateDevices;
+            (navigator.mediaDevices as any).enumerateDevices = undefined;
+            try {
+                await enumerateDevices();
+                expect.fail('Should have thrown due to missing enumerateDevices');
+            } catch (err) {
+                expect(err).to.exist;
+            } finally {
+                (navigator.mediaDevices as any).enumerateDevices = origEnumerateDevices;
+            }
+        });
         it('returns a Promise', () => {
             expect(enumerateDevices()).to.be.a('Promise');
         });
@@ -16,7 +27,18 @@ describe('mediaDevices (browser)', () => {
         });
     });
     describe('getUserMedia', () => {
-        it('TODO: rejects with an error if getUserMedia is not supported');
+        it('rejects with an error if getUserMedia is not supported', async () => {
+            const origGetUserMedia = navigator.mediaDevices.getUserMedia;
+            (navigator.mediaDevices as any).getUserMedia = undefined;
+            try {
+                await getUserMedia({});
+                expect.fail('Should have thrown due to missing getUserMedia');
+            } catch (err) {
+                expect(err).to.exist;
+            } finally {
+                (navigator.mediaDevices as any).getUserMedia = origGetUserMedia;
+            }
+        });
         it('returns a Promise', () => {
             expect(getUserMedia({})).to.be.a('Promise');
         });
