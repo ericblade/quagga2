09db004113e786007cda908f5c71ba01580f4a96
Eric Blade <blade.eric@gmail.com>
2025-12-19 22:15:01 -0500

    hmm. attempt to adjust webpack config to deal with node problems

--- configs/webpack.node.config.js
+++ configs/webpack.node.config.js
@@ -10,7 +10,18 @@ module.exports = {
         'http',
         'https',
         'url',
+        'sharp',
     ],
+};
+module.exports.target = 'node';
+module.exports.resolve = {
+    ...module.exports.resolve,
+    // Prefer Node builds of packages (e.g., ndarray-pixels) instead of browser entries
+    mainFields: ['main', 'module'],
+    alias: {
+        // Force ndarray-pixels to resolve to its Node build; webpack 4 lacks conditionNames support
+        'ndarray-pixels$': 'ndarray-pixels/dist/ndarray-pixels-node.cjs',
+    },
 };
 module.exports.output.libraryTarget = 'commonjs';
 module.exports.output.library = undefined;
